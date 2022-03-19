20a1b7da2f92373894ca85989cb002a44be2d44a
tanguylfcd <tanguy.lafourcade@icloud.com>
2022-03-19 21:19:16 +0100

    mobile & tablet

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -3,6 +3,11 @@ window.addEventListener("DOMContentLoaded", function () {
   let menu = document.querySelector(".header__menu");
 
   burger.addEventListener("click", function () {
-    menu.classList.toggle("menu_open");
+    menu.classList.toggle("header__menu--open");
+    if (menu.classList.contains("header__menu--open")) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
   });
 });
