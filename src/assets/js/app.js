afa27cd376a7495e7a468636d7dd0b2c69704fb0
tanguylfcd <tanguy.lafourcade@icloud.com>
2022-03-20 23:18:45 +0100

    JS animation

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,4 +1,6 @@
 window.addEventListener("DOMContentLoaded", function () {
+  gsap.registerPlugin(ScrollTrigger);
+
   let burger = document.getElementById("burger");
   let menu = document.querySelector(".header__menu");
 
@@ -9,5 +11,18 @@ window.addEventListener("DOMContentLoaded", function () {
     } else {
       document.body.style.overflow = "auto";
     }
+  });
+
+  sections = document.querySelectorAll(".sectionAnim");
+  sections.forEach((section) => {
+    gsap.from(section, {
+      y: 500,
+      scrollTrigger: {
+        trigger: section,
+        //markers: true,
+        start: "-500px 70%",
+        end: "-400px 60%",
+      },
+    });
   });
 });
